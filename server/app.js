2e5b22d9a4072e76cdc1bc9d39aaf67e20c94a91
Ronnie Kaito Imagawa <ronnie.imagawa@cccareers.org>
2024-09-18 19:28:49 -0400

    finish remaining endpoints

--- server/app.js
+++ server/app.js
@@ -19,12 +19,15 @@ const initialData = [
   },
 ];
 
+const bodyParser = require("body-parser");
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   const obj = { status: "ok" };
@@ -33,6 +36,47 @@ app.get("/", (req, res) => {
 
 app.get("/api/TodoItems", (req, res) => {
   res.send(initialData).status(200);
+});
+
+app.get("/api/TodoItems/:number", (req, res) => {
+  try {
+    const todoId = Number(req.params.number);
+    const todoItem = initialData.find((item) => item.todoItemId === todoId);
+
+    if (!todoItem) {
+      throw new Error("Todo item was not found");
+    }
+    res.json(todoItem).status(200);
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.post("/api/TodoItems", (req, res) => {
+  const { todoItemId, name, priority, completed } = req.body;
+
+  const newTodo = {
+    todoItemId,
+    name,
+    priority,
+    completed,
+  };
+
+  res.status(201).json(newTodo);
+});
+
+app.delete("/api/TodoItems/:number", (req, res) => {
+  try {
+    const todoId = Number(req.params.number);
+    const todoItem = initialData.find((item) => item.todoItemId === todoId);
+
+    if (!todoItem) {
+      throw new Error("Todo item was not found");
+    }
+    res.json(todoItem).status(200);
+  } catch (error) {
+    throw error;
+  }
 });
 
 module.exports = app;
